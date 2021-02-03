a5e71b26239921c2db4e9d523eb8b24633b4c99c
Fure <Fure@web-2.local>
2021-02-03 13:00:00 +0300

    Initial Commit

---
+++ Pryaniky/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pryaniky
+//
+//  Created by Fure on 2/3/21.
+//  Copyright © 2021 NotifyMe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
